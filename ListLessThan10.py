b4349d36dc234958d5989308c5a55407476af537
nshagam <31753685+nshagam@users.noreply.github.com>
2017-09-09 14:32:49 -0500

    Add files via upload

---
+++ ListLessThan10.py
@@ -0,0 +1,36 @@
+# Take a list, say for example this one:
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# and write a program that prints out all the elements of the list that are less than 5.
+
+#Extras:
+
+# 1. Instead of printing the elements one by one, make a new list that has all the elements less than
+# 5 from this list in it and print out this new list.
+
+# 2. Write this in one line of Python.
+
+# 3. Ask the user for a number and return a list that contains only elements from the
+# original list a that are smaller than that number given by the user.
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+
+for element in a:
+    if element < 5:
+        print(element)
+
+newlist = []
+for element in a:
+    if element < 5:
+        newlist.append(element)
+print(newlist)
+
+lessThan5List = [i for i in a if i < 5]
+print(lessThan5List)
+
+num = int(input("Enter a number: "))
+lessThanNum = [i for i in a if i < num]
+print(lessThanNum)
+
+
+
